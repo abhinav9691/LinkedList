74cb499af8128edc779c75cb7645a8201ae978f2
Abhinav Kumar <kabhinav713@gmail.com>
2023-06-21 18:22:05 +0530

    [Abhinav]-adding linkedlist elements

---
+++ LinkedList1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace LinkedList
+{
+    public class LinkedList1
+    {
+        public Node head;
+        public void AddList(int data)
+        {
+            Node newnode = new Node(data);
+            if (head == null)
+            {
+                head = newnode;
+                Console.WriteLine("{0} new Node inserted into the LinkedList", data);
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = newnode;
+            }
+            Console.WriteLine("{0} new Node inserted into the LinkedList", newnode.data);
+        }
+
+        public void Display()
+        {
+            Node temp = head;
+            if (temp == null)
+            {
+                Console.WriteLine("LinkedList is Empty");
+            }
+            else
+            {
+                while (temp != null)
+                {
+                    Console.WriteLine(temp.data);
+                    temp = temp.next;
+                }
+
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿using LinkedList;
+using System.Collections.Generic;
+
+LinkedList1 linkedList = new LinkedList1();
+linkedList.AddList(56);
+linkedList.AddList(30);
+linkedList.AddList(70);
+linkedList.Display();
